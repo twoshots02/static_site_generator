586393d613a2012a5b71432e56efbb8cca52357f
twoshots02 <twoshots02@gmail.com>
2025-01-27 21:31:03 -0600

    created leafnode in htmlnode.py as well as testcases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -4,11 +4,10 @@ class HTMLNode():
     def __init__ (self, tag = None, value = None, children = None, props = None):
         self.tag = tag
         self.value = value
-        self.children = children
-        if not props:
-            self.props = {}
-        else:
-            self.props = props
+        if type(self) is not LeafNode:  # Only set children for non-LeafNodes
+            self.children = children or []
+        
+        self.props = props or {}
     
     def to_html(self):
         raise NotImplementedError
@@ -21,4 +20,24 @@ class HTMLNode():
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, props = props)
+        self._children = ()
+
+    @property
+    def children(self):
+        return self._children
     
+    @children.setter
+    def children(self, value):
+        raise AttributeError("Cannot set children on a LeafNode")
+    
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode must have a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -17,6 +17,26 @@ class TestHtmlNode(unittest.TestCase):
             "target": "_blank"
         })
         self.assertEqual(node.props_to_html(), ' href="https://www.boot.dev" target="_blank"')
+
+class TestLeafNode(unittest.TestCase):
+    def test_children(self):
+        node = LeafNode("a", "This is a link", props={"href": "https://www.boot.dev"})
+        self.assertEqual(node.children, ())
+        with self.assertRaises(AttributeError):
+            node.children = "This is a link"
+
+    def test_to_html(self):
+        node = LeafNode("a", "This is a link", props={"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev">This is a link</a>')
+
+    def test_to_html_no_value(self):
+        node = LeafNode("a", None, props={"href": "https://www.boot.dev"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "This is a link", props={"href": "https://www.boot.dev"})
+        self.assertEqual(node.to_html(), 'This is a link')
 
 if __name__ == "__main__":
     unittest.main()
